42cf910022a7e2592f9261efe28eceb17119b0da
Brennan Woodbury <bthedub@gmail.com>
2024-11-10 15:22:16 -0700

    changed navbar

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -7,11 +7,11 @@ export default function Navbar(): React.ReactNode {
       <a href={"/"}><img src={"/svg/terminal.svg"} alt="terminal"/></a>
       <div className={styles.spacer}></div>
       <ul>
-        <li><a href={"https://www.linkedin.com/in/brennan-woodbury-549435bb"} target={"_blank"}>LinkedIn</a></li>
-        <li><a href={"https://www.github.com/BrennanWoodbury"} target={"_blank"}>Github </a></li>
         <li><a href={"/"}>Home</a></li>
         <li><a href={"/contact"}>Contact</a></li>
         <li><a href={"/projects"}>Projects</a></li>
+        <li><a href={"https://www.linkedin.com/in/brennan-woodbury-549435bb"} target={"_blank"}>LinkedIn</a></li>
+        <li><a href={"https://www.github.com/BrennanWoodbury"} target={"_blank"}>Github </a></li>
       </ul>
 
     </div>
